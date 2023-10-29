91160f295421ce340a5b9ccaccb95e7ba00c7ad8
Rafnas <rafnas71@gmail.com>
2023-10-29 14:04:55 +0530

    homepage layout

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,12 +1,12 @@
 export default function Cards() {
   return (
-    <div className="flex w-40h-40">
+    <div className="flex max-wd-sm max-h-sm">
       <div className="bg-slate-200 p-6 gap-1">
         <h5 className="mb-2 text-xl">Pokemon</h5>
         <p className="mb-4">
           Some quick example text to build on the card 
         </p>
-        <button type="button" className="bg-black text-white rounded-full borde">
+        <button type="button" className="bg-black p-2 text-white rounded-full border">
           Readmore
         </button>
       </div>
